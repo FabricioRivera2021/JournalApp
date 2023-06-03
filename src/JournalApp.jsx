fb0817fe2dfe1bbe9bbf17f1e23f1e530eba4486
unknown <fabricio.rivera2012@gmail.com>
2023-06-03 10:09:42 -0300

    primer commit

---
+++ src/JournalApp.jsx
@@ -0,0 +1,12 @@
+import { AppRouter } from "./router/AppRouter"
+import { BrowserRouter } from "react-router-dom"
+
+
+
+export const JournalApp = () => {
+  return (
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  )
+}
