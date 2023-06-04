55ee73fd234dd13323fd1eae5cbd8194e2a88375
unknown <fabricio.rivera2012@gmail.com>
2023-06-04 15:04:37 -0300

    terminando el diseño

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,8 @@
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+// eslint-disable-next-line no-unused-vars
 import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 
 export const JournalPage = () => {
@@ -10,9 +13,30 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique mollitia eligendi deserunt optio suscipit omnis sunt, ab necessitatibus impedit soluta dolorum nisi magnam ex nam officia ipsa dolores, recusandae illum!</Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {
+            backgroundColor: 'error.main',
+            opacity: 0.9
+          },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined
+          sx={{
+            fontSize: 30
+          }} 
+        />
+      </IconButton>
+
     </JournalLayout>
   )
 }
