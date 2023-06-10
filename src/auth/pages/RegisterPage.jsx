b51c33b2d1d56ca0dd2d6849c4563655b5c861f4
unknown <fabricio.rivera2012@gmail.com>
2023-06-10 15:42:11 -0300

    validando el formulario

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,26 +1,90 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Button, Link, Grid, TextField, Typography } from "@mui/material"
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
 
+
+const formData = {
+  email: '',
+  password: '',
+  confirmPass: '',
+  displayName: ''
+}
+
+/*Validaciones personalizadas, se le pasa como parametro a el useForm, como segundo parametro */
+const formValidation = {
+  email: [ (value) => value.includes('@'), 'El correo debe contener un signo de @'],
+  password: [ (value) => value.length >= 6, 'El password debe de tener mas de 6 caracteres'],
+  confirmPass: [ (value) => value.length >= 6, 'El password debe de tener mas de 6 caracteres'],
+  displayName: [ (value) => value.length >= 1, 'El nombre es obligatorio'],
+}
 
 export const RegisterPage = () => {
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const { formState, displayName, email, password, confirmPass, onInputChange, 
+          isFormValid, displayNameValid, emailValid, passwordValid, confirmPassValid
+        } = useForm(formData, formValidation);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // console.log(formState);
+  }
+
   return (
-    <AuthLayout title="Register">
-    <form action="">
+  <AuthLayout title="Register">
+    <h1>Formulario valido: {(isFormValid) ? 'Valido' : 'no valido'}</h1>
+    <form onSubmit={onSubmit}>
 
       <Grid container>
         {/* inputs */}
         <Grid item xs={12} sx={{marginBottom: 2}}>
-          <TextField label='Nombre completo' type='text' placeholder="..." fullWidth/>
+          <TextField label='Nombre completo' 
+                     type='text' 
+                     placeholder="..." 
+                     fullWidth name="displayName" 
+                     value={displayName} 
+                     onChange={onInputChange}
+                     error={ !!displayNameValid }//se usa doble negacion para q no tire el error de q es un string el chinguenguecncha
+                     helperText={ displayNameValid }
+          />
         </Grid>
         <Grid item xs={12} sx={{marginBottom: 2}}>
-          <TextField label='Mail' type='mail' placeholder="correo@correo.com" fullWidth/>
+          <TextField label='Mail' 
+                     type='mail' 
+                     placeholder="correo@correo.com" 
+                     fullWidth 
+                     name="email" 
+                     value={email} 
+                     onChange={onInputChange}
+                     error={ !!emailValid }
+                     helperText={ emailValid }
+          />
         </Grid>
         <Grid item xs={12} sx={{marginBottom: 2}}>
-          <TextField label='Contraseña' type='password' fullWidth/>
+          <TextField label='Contraseña' 
+                     type='password' 
+                     fullWidth 
+                     name="password" 
+                     value={password} 
+                     onChange={onInputChange}
+                     error={ !!passwordValid }
+                     helperText={ passwordValid }
+          />
         </Grid>
         <Grid item xs={12}>
-          <TextField label='Confirmar contraseña' type='password' fullWidth/>
+          <TextField label='Confirmar contraseña' 
+                     type='password' 
+                     fullWidth 
+                     name="confirmPass" 
+                     value={confirmPass} 
+                     onChange={onInputChange}
+                     error={ !!confirmPassValid }
+                     helperText={ confirmPassValid }
+          />
         </Grid>
 
         {/* Buttons */}
@@ -28,7 +92,7 @@ export const RegisterPage = () => {
           spacing={2}
           sx={{marginTop: 1, marginBottom: 2}}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" fullWidth type="submit">
                 Registrarme
               </Button>
             </Grid>
@@ -45,6 +109,6 @@ export const RegisterPage = () => {
       </Grid>
 
     </form>
-</AuthLayout>
+  </AuthLayout>
   )
 }
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,10 +1,29 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import { useEffect, useMemo, useState } from "react";
 
 /* eslint-disable no-undef */
 
-export const useForm = (initialForm = {}) => {//por defecto el form es un objeto
+export const useForm = (initialForm = {}, formValidations = {}) => {//por defecto el form es un objeto
 
     const [formState, setFormState] = useState(initialForm);
+
+    /*Validacion personalizada */
+    const [formValidation, setFormValidation] = useState({})
+
+    useEffect(() => {
+
+      createValidators();
+
+    }, [formState])
+    
+    const isFormValid = useMemo( () => {
+      for (const item of Object.keys(formValidation)) {
+        if (formValidation[item] !== null) return false;
+      }
+
+      return true;
+    }, [formValidation])
   
     const onInputChange = ({ target }) => {
       const { name, value } = target;
@@ -19,11 +38,30 @@ export const useForm = (initialForm = {}) => {//por defecto el form es un objeto
     const onResetForm = () => {
       setFormState(initialForm)
     }
+
+    /*Validacion personalizada */
+    const createValidators = () => {
+
+      const formCheckedValues = {}
+
+      for (const formField of Object.keys(formValidations)) {
+        const [ fn, errorMessage ] = formValidations[formField];
+
+        formCheckedValues[`${formField}Valid`] = fn(formState[formField]) ? null : errorMessage;
+        formCheckedValues[`confirmPassValid`] = (formState.password === formState.confirmPass) ? null : 'Las contraseñas deben ser iguales';
+      }
+      // console.log(formCheckedValues)
+      setFormValidation(formCheckedValues);
+    }
+
   
     return {
       ...formState,
       formState,
       onInputChange,
-      onResetForm
+      onResetForm,
+
+      ...formValidation,
+      isFormValid
     };
   };
